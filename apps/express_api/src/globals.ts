80f30b3bcde8dcd13a441ede1032f4976b2edb1c
Saumya Patil <saumyasachinpatil@gmail.com>
2023-08-09 19:03:46 +0530

    require ssl

--- apps/express_api/src/globals.ts
+++ apps/express_api/src/globals.ts
@@ -8,8 +8,8 @@ import {
 export let application: Application | null = null;
 
 export function init_postgres(database_url: string, secret_key: string) {
-    
-    const database = new PostgresDatabase(database_url);
+
+    const database = new PostgresDatabase(database_url + "?sslmode=require");
     const usersTable = new PgUsersTable(database);
     const authTokenGenerator = new JWTAuthTokenGenerator(secret_key);
 
