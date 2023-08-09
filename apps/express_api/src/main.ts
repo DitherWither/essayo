0aef4b9260b9b19354fd3f9eec9d2501773b77e5
Vardhan Patil <demonslayervardhan@gmail.com>
2023-08-09 19:25:07 +0530

    print list of routes on startup

--- apps/express_api/src/main.ts
+++ apps/express_api/src/main.ts
@@ -22,6 +22,49 @@ init_postgres(databaseUrl, secretKey);
 app.use(express.json());
 app.use("/api/v1", appRouter);
 
+// START OF DEBUG STATEMENTS
+
+function print(path: any, layer: any) {
+    if (layer.route) {
+        layer.route.stack.forEach(
+            print.bind(null, path.concat(split(layer.route.path)))
+        );
+    } else if (layer.name === "router" && layer.handle.stack) {
+        layer.handle.stack.forEach(
+            print.bind(null, path.concat(split(layer.regexp)))
+        );
+    } else if (layer.method) {
+        console.log(
+            "%s /%s",
+            layer.method.toUpperCase(),
+            path.concat(split(layer.regexp)).filter(Boolean).join("/")
+        );
+    }
+}
+
+function split(thing: any) {
+    if (typeof thing === "string") {
+        return thing.split("/");
+    } else if (thing.fast_slash) {
+        return "";
+    } else {
+        var match = thing
+            .toString()
+            .replace("\\/?", "")
+            .replace("(?=\\/|$)", "$")
+            .match(
+                /^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//
+            );
+        return match
+            ? match[1].replace(/\\(.)/g, "$1").split("/")
+            : "<complex:" + thing.toString() + ">";
+    }
+}
+
+app._router.stack.forEach(print.bind(null, []));
+
+// END OF DEBUG STATEMENTS
+
 const port = process.env.PORT || 3003;
 
 app.listen(port, () => {
